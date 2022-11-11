9909f1c2e4d93726431ab26a7208906996215f81
graziacrp <graziacarpineta25@gmail.com>
2022-11-11 16:47:00 +0100

    ok

--- script.js
+++ script.js
@@ -1,294 +1,42 @@
 
 
-let containerPrincipale = document.querySelector(".mio-container");
+let Numeri = 0;
+let list = document.getElementById("list");
+let elementi ="";
+let n = 0;
 
 
-for (let i = 0; i < 100; i++) { }
+for( let i = 0; i < 100 ; i++){
+  n ++;
+  Numeri = i+ 1;
 
+  if ((Numeri % 3 == 0) && (Numeri % 5 != 0)){
 
-console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
-console.log(5);
-console.log(6);
-console.log(7);
-console.log(8);
-console.log(9);
-console.log(10);
-console.log(11);
-console.log(12);
-console.log(13);
-console.log(14);
-console.log(15);
-console.log(16);
-console.log(17);
-console.log(18);
-console.log(19);
-console.log(20);
-console.log(21);
-console.log(22);
-console.log(23);
-console.log(24);
-console.log(25);
-console.log(26);
-console.log(27);
-console.log(28);
-console.log(29);
-console.log(30);
-console.log(31);
-console.log(32);
-console.log(33);
-console.log(34);
-console.log(35);
-console.log(36);
-console.log(37);
-console.log(38);
-console.log(39);
-console.log(40);
-console.log(41);
-console.log(42);
-console.log(43);
-console.log(44);
-console.log(45);
-console.log(46);
-console.log(47);
-console.log(48);
-console.log(49);
-console.log(50);
-console.log(51);
-console.log(52);
-console.log(53);
-console.log(54);
-console.log(55);
-console.log(56);
-console.log(57);
-console.log(58);
-console.log(59);
-console.log(60);
-console.log(61);
-console.log(62);
-console.log(63);
-console.log(64);
-console.log(65);
-console.log(66);
-console.log(67);
-console.log(68);
-console.log(69);
-console.log(70);
-console.log(71);
-console.log(72);
-console.log(73);
-console.log(74);
-console.log(75);
-console.log(76);
-console.log(77);
-console.log(78);
-console.log(79);
-console.log(80);
-console.log(81);
-console.log(82);
-console.log(83);
-console.log(84);
-console.log(85);
-console.log(86);
-console.log(87);
-console.log(88);
-console.log(89);
-console.log(90);
-console.log(91);
-console.log(92);
-console.log(93);
-console.log(94);
-console.log(95);
-console.log(96);
-console.log(97);
-console.log(98);
-console.log(99);
-console.log(100);
+    Numeri = "(Fizz)" + n;
+    console.log("Fizz      "+ n);
 
+  } else if ((Numeri % 5 == 0) && (Numeri % 3 != 0)){
+   
+    Numeri = `(Buzz)${n}`;
+    console.log("Buzz      " + n );
 
+    
+  } else if ((Numeri % 3 == 0) && (Numeri % 5 == 0)){
 
+    Numeri = `(FizzBuzz)${n}`;
+    console.log("FizzBuzz " + n);
+    
+  } else {
+    Numeri = "(numeri senza modifiche)" + n;
+    console.log("numeri senza modifiche" + n);
+    
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-if (i = 3) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 6) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 9) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 12) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 15) {
-    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
-} else if (i = 18) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 21) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 24) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 27) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 30) {
-    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
-} else if (i = 33) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 36) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 39) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 42) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 45) {
-    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
-} else if (i = 48) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 51) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 54) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 57) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 60) {
-    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
-} else if (i = 63) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 66) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 69) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 72) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 75) {
-    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
-} else if (i = 78) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 81) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 84) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 87) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 90) {
-    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
-} else if (i = 93) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 96) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
-} else if (i = 99) {
-    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+  elementi += "<li>" + Numeri + "</li>";
+  
 }
+list.innerHTML= elementi;
 
 
-switch (i) {
-    case 3:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 6:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 9:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 12:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 15:
-        containerPrincipale.innerHTML = `<div class="square"><p>FizzBuzz</p></div>`;
-        break;
-    case 18:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 21:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 24:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 27:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 30:
-        containerPrincipale.innerHTML = `<div class="square"><p>FizzBuzz</p></div>`;
-        break;
-    case 33:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 36:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 39:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 42:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 45:
-        containerPrincipale.innerHTML = `<div class="square"><p>FizzBuzz</p></div>`;
-        break;
-    case 48:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 51:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 54:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 57:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 60:
-        containerPrincipale.innerHTML = `<div class="square"><p>FizzBuzz</p></div>`;
-        break;
-    case 63:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 66:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 69:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 72:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 75:
-        containerPrincipale.innerHTML = `<div class="square"><p>FizzBuzz</p></div>`;
-        break;
-    case 78:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 81:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 84:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 87:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 90:
-        containerPrincipale.innerHTML = `<div class="square"><p>FizzBuzz</p></div>`;
-        break;
-    case 93:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 96:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-    case 99:
-        containerPrincipale.innerHTML = `<div class="square"><p>Fizz</p></div>`;
-        break;
-}                                           
+
+
