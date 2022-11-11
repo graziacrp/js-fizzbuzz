6e95c3bca7810440342af3458750d013da2863f5
graziacrp <graziacarpineta25@gmail.com>
2022-11-11 15:09:44 +0100

    ok

--- script.js
+++ script.js
@@ -4,3 +4,82 @@ let containerPrincipale=document.querySelector(".mio-container");
 for (let i=0; i < 100; i++) {}
 
 
+console.log (10);
+console.log (20);
+console.log (30);
+console.log (40);
+console.log (50);
+console.log (60);
+console.log (70);
+console.log (80);
+console.log (90);
+console.log (100);
+
+
+if(i == 3){
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 6){
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 9){
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 12){
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 15){
+    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
+} else if (i == 18){
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 21){
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if(i == 24) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 27) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 30) {
+    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
+} else if (i == 33) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 36) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 39) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`; 
+} else if (i == 42) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`; 
+} else if (i == 45 ) {
+    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
+} else if (i == 48 ) {
+   containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 51) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 54) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 57) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if ( i == 60) {
+    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
+} else if ( i == 63) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 66) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if ( i == 69) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if ( i == 72) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 75) {
+    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
+} else if (i == 78 ) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 81 ) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 84) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 87) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 90) {
+    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
+} else if (i == 93) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 96) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+} else if (i == 99) {
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
+}
